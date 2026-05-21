bb4d5ce6aa05b72aca41377e51d1a959f485e910
Asmodeus <kirill6satur@gmail.com>
2026-05-21 10:31:49 +0300

    One more Clouser

--- src/index.js
+++ src/index.js
@@ -15,4 +15,8 @@ function createFilter(callback){
 
 const getOddNumbers = createFilter((el) => el % 2 !== 0);
 
+const getNumbersBigThenthree = createFilter((el) => el > 3);
+
 console.log(getOddNumbers([1, 2, 3, 4, 5]));
+
+console.log(getNumbersBigThenthree([1, 2, 3, 4, 5]));
